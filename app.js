cf30547cfa413004e240f47906e19b23b91d32f7
Anoush khan <143010760+Anoush455@users.noreply.github.com>
2023-10-09 23:10:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// var showButton = document.getElementById('showButton');
+// var elementToHide = document.getElementById('elementToHide');
+// showButton.addEventListener('click', function() {
+//   if (elementToHide.style.display === 'none') {
+//     elementToHide.style.display = 'block';
+//   } else {
+//     elementToHide.style.display = 'none';
+//   }
+// });
+
+// var myButton = document.getElementById('myButton');
+// myButton.addEventListener('click', function() {
+//   alert('Button clicked!');
+// });
+
+// var myElement = document.getElementById('myElement');
+// myElement.addEventListener('mouseover', function() {
+//   alert('Mouse over the element!');
+// });
+
+// var userInput = document.getElementById('userInput');
+// var submitButton = document.getElementById('submitButton');
+// submitButton.addEventListener('click', function() {
+//   const inputValue = userInput.value;
+//   console.log('User input: ' + inputValue);
+// });
+
+// var selectBox = document.getElementById('selectBox');
+// var submitButton = document.getElementById('submitButton');
+// submitButton.addEventListener('click', function() {
+//   var selectedValue = selectBox.value;
+//   console.log('Selected value: ' + selectedValue);
+// });
+
+
+// var maritalStatusCheckbox = document.getElementById('maritalStatus');
+// var statusField = document.getElementById('status');
+// var submitButton = document.getElementById('submitButton');
+
+// submitButton.addEventListener('click', function() {
+//   var married = maritalStatusCheckbox.checked;
+//   if (!married) {
+//     statusField.value = 'available';
+//   }
+// });
+
+// var userInput = document.getElementById('userInput');
+// var submitButton = document.getElementById('submitButton');
+
+// submitButton.addEventListener('click', function() {
+//   if (userInput.value.trim() === '') {
+//     userInput.value = 'Default Text';
+//   }
+// });
+
